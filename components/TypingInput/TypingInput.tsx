a68797d6958ddfae3a374d3d49a4ff5410abc995
Kostiantyn Huzenko <huzenko.c@gmail.com>
2025-02-18 19:02:57 -0500

    adding typing error state

--- components/TypingInput/TypingInput.tsx
+++ components/TypingInput/TypingInput.tsx
@@ -5,6 +5,8 @@ import { useEffect, useRef, useState } from "react";
 import { Text } from "../ui/text";
 import * as Speech from "expo-speech";
 
+const ERROR_COUNT_LIMIT = 3;
+
 interface TypingInputProps {
   currentWord: TypingWords;
 }
@@ -12,14 +14,25 @@ interface TypingInputProps {
 export const TypingInput = ({ currentWord }: TypingInputProps) => {
   const splitWord = currentWord.word.split("");
   const [currentActiveIndex, setCurrentActiveIndex] = useState(0);
+  const [errorCount, setErrorCount] = useState(1);
+  const [isError, setIsError] = useState(false);
   const inputRef = useRef<TextInput>(null);
 
   useEffect(() => {
     Speech.speak(currentWord.word);
   }, [currentWord]);
 
+  useEffect(() => {
+    if (isError) {
+      setTimeout(() => {
+        setIsError(false);
+      }, 1000);
+    }
+  }, [isError]);
+
   return (
     <View className="flex flex-col items-center justify-center w-full mb-6">
+      {isError && <Text>error</Text>}
       <Input
         ref={inputRef}
         focusable={false}
@@ -36,8 +49,15 @@ export const TypingInput = ({ currentWord }: TypingInputProps) => {
           const currentLetter = splitWord[currentActiveIndex];
 
           if (text === currentLetter) {
+            setErrorCount(1);
             setCurrentActiveIndex((prev) => prev + 1);
+            return;
           }
+
+          if (errorCount >= ERROR_COUNT_LIMIT) {
+          }
+          setIsError(true);
+          setErrorCount((prev) => prev + 1);
         }}
       />
       <View
