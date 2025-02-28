6edf012a97b006d848d61b7ee0977aa24560569a
Kostiantyn Huzenko <huzenko.c@gmail.com>
2025-02-28 02:48:52 -0500

    added reanimated and changed keyboard animation

--- components/Keyboard/Keyboard.tsx
+++ components/Keyboard/Keyboard.tsx
@@ -2,6 +2,15 @@ import { View } from "react-native";
 import { Text } from "../ui/text";
 import { Button } from "../ui/button";
 import clsx from "clsx";
+import Animated, {
+  Easing,
+  ReduceMotion,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import { useState } from "react";
 
 const KEYBOARD_ROWS = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
@@ -14,25 +23,62 @@ export interface KeyboardProps {
 }
 
 export const Keyboard = ({ onKeyboardPress }: KeyboardProps) => {
+  const top = useSharedValue(0);
+  const [pressedRow, setPressedRow] = useState(0);
+  const [pressedKey, setPressedKey] = useState(0);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: top.value }],
+  }));
+
   return (
-    <View className="flex flex-2 flex-col items-center justify-center w-full mb-10">
+    <View className="flex flex-2 flex-col items-center justify-center w-full mb-8">
       {KEYBOARD_ROWS.map((row, index) => (
-        <View key={index} className="flex flex-row justify-center">
+        <View key={index} className="flex flex-row justify-center w-full">
           {row.map((key, keyIndex) => (
-            <Button
-              key={`${key}-${keyIndex}`}
-              size="lg"
-              variant="outline"
+            <Animated.View
               className={clsx(
-                " w-full max-w-12 h-12 flex justify-center items-center text-center flex-1 p-1 m-px border border-zinc-800 rounded-md active:translate-y-[-40px] active:z-10 transition duration-100",
-                { "opacity-0 pointer-events-none": key === "" },
-                { "left-[-5%]": index === 1 },
-                { "left-[-5%]": index === 2 }
+                "flex flex-1 justify-center items-center max-w-12 h-12 m-px",
+                {
+                  "left-[-5%]": index === 1 || index === 2,
+                }
               )}
-              onPress={() => onKeyboardPress(key)}
+              key={`${key}-${keyIndex}`}
+              style={
+                pressedRow === index && keyIndex === pressedKey
+                  ? animatedStyle
+                  : {}
+              }
             >
-              <Text className="">{key}</Text>
-            </Button>
+              {key !== "" ? (
+                <Button
+                  size="sm"
+                  variant="outline"
+                  className={clsx("flex w-full h-full", {
+                    "opacity-0 pointer-events-none": key === "",
+                  })}
+                  onPressIn={() => {
+                    setPressedRow(index);
+                    setPressedKey(keyIndex);
+                    onKeyboardPress(key);
+                    {
+                      top.value = withTiming(top.value - 40, {
+                        duration: 50,
+                        easing: Easing.linear,
+                      });
+                    }
+                  }}
+                  onPressOut={() => {
+                    top.value = withTiming(0, {
+                      duration: 10,
+                      easing: Easing.linear,
+                    });
+                  }}
+                >
+                  <Text className="">{key}</Text>
+                </Button>
+              ) : null}
+            </Animated.View>
           ))}
         </View>
       ))}
