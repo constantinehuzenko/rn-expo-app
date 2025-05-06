13e2dcde63a4ec015b9820b2e4ef3447182ab36e
Kostiantyn Huzenko <huzenko.c@gmail.com>
2025-05-05 22:12:17 -0400

    basic supabase integration

--- app/(tabs)/folders.tsx
+++ app/(tabs)/folders.tsx
@@ -10,19 +10,66 @@ import {
 } from "@/components/ui/card";
 import { Text } from "@/components/ui/text";
 import { useGlobalState } from "@/storage/global";
+import { useSession, useUser } from "@clerk/clerk-expo";
+import { createClient } from "@supabase/supabase-js";
 import { router } from "expo-router";
-import Animated from 'react-native-reanimated';
+import { useEffect, useState } from "react";
+import Animated from "react-native-reanimated";
 
 export default function Folders() {
-  const {
-    setCurrentFolderIdLocal,
-    foldersLocal,
-    setCurrentWordIdLocal,
-  } = useGlobalState();
+  const { setCurrentFolderIdLocal, foldersLocal, setCurrentWordIdLocal } =
+    useGlobalState();
+  const [folders, setFolders] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const { user } = useUser();
+  const { session } = useSession();
+
+  function createClerkSupabaseClient() {
+    const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+    const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseAnonKey) {
+      console.error("Supabase environment variables are missing!");
+      throw new Error("Supabase configuration is incomplete");
+    }
+
+    return createClient(supabaseUrl, supabaseAnonKey, {
+      async accessToken() {
+        return session?.getToken() ?? null;
+      },
+    });
+  }
+
+  const client = createClerkSupabaseClient();
+
+  useEffect(() => {
+    if (!user) return;
+
+    async function loadTasks() {
+      setLoading(true);
+      const { data, error } = await client.from("folders").select();
+      if (!error) setFolders(data);
+      setLoading(false);
+    }
+
+    loadTasks();
+  }, [user]);
 
   return (
     <PageWrapper>
       <Animated.ScrollView className="w-full flex">
+        <Button
+          onPress={async () => {
+            await client.from("folders").insert({
+              name: "db new test koss",
+            });
+          }}
+        >
+          <Text>some</Text>
+        </Button>
+        {loading ? <Text>Loading...</Text> : null}
+        {folders.map(({name}) => <Text>{name}</Text>)}
         {foldersLocal.map(({ name, description, words, id }) => (
           <Card className="w-full mb-4" key={id}>
             <CardHeader>
